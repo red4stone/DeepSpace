23fb8be5153f85915079123b9090f052089e86e0
Tom Gagne <tggagne@gmail.com>
2019-02-09 13:00:53 -0500

    First cut at CompressorSubsystem

---
+++ src/main/java/frc/robot/subsystems/CompressorSubsystem.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+/*
+see compressor doc at
+
+http://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/Compressor.html
+*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class CompressorSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  Compressor aCompressor  = new Compressor();
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  @Override
+  public void periodic()
+  {
+    if (RobotController.getBatteryVoltage() < 11.0) {
+      stop();
+    }
+
+    if (RobotController.getBatteryVoltage() > 11.5) {
+      stop();
+    }
+  }
+
+  // enable/disable the auto function
+  public void start() {
+    aCompressor.start();
+  }
+
+  public void stop() {
+    aCompressor.stop();
+  }
+
+  // returns electrical current measured in amps
+  public double getCurrentAmps() {
+    return aCompressor.getCompressorCurrent();
+  }
+
+  public Boolean isOn() {
+    return aCompressor.getPressureSwitchValue();
+  }
+
+  public Boolean isOff() {
+    return !isOn();
+  }
+}
